0edfc5ccda72d4ae815c27f1b6e85425c0298acb
alaurin10 <50841025+alaurin10@users.noreply.github.com>
2022-05-12 11:32:38 -0700

    Most recent robot code as of 5/12
    Updated to control GPIO pins with digitalio

--- RobotTestCode/TraceSquare.py
+++ RobotTestCode/TraceSquare.py
@@ -1,59 +1,111 @@
 import time
 import board
 import busio
+import digitalio
+import Jetson.GPIO as GPIO
 import adafruit_pca9685
-
+ 
 # Initialize i2c bus and connect to servo driver board
 i2c = busio.I2C(board.SCL, board.SDA)       # i2c = busio.I2C(board.D3, board.D2)
 pca = adafruit_pca9685.PCA9685(i2c)
-
+ 
 # Set frequency to 60 Hz
-pca.frequency = 60
-
-# Initialize which channels each motor is connencted to
-motorR1 = pca.channels[0]
-motorR2 = pca.channels[1]
-
-motorL1 = pca.channels[2]
-motorL2 = pca.channels[3]
-
-motor_channels = [motorR1, motorR2, motorL1, motorL2]
+pca.frequency = 100
+ 
+# Initialize PWM channels
+ENA = pca.channels[0]
+ENB = pca.channels[1]
 
 
-# Function to make the robot stop
+# Initialize motor control pins
+IN1 = digitalio.DigitalInOut(board.D12)
+IN1.direction = digitalio.Direction.OUTPUT
+
+IN2 = digitalio.DigitalInOut(board.D13)
+IN2.direction = digitalio.Direction.OUTPUT
+
+IN3 = digitalio.DigitalInOut(board.D19)
+IN3.direction = digitalio.Direction.OUTPUT
+
+IN4 = digitalio.DigitalInOut(board.D26)
+IN4.direction = digitalio.Direction.OUTPUT
+ 
+# Function to move right wheel
+def move_right_wheel(speed):
+    pwm = int(0xFFFF * speed)
+ 
+    # Move forward
+    if speed > 0:
+        IN1.value = True
+        IN2.value = False
+
+        ENA.duty_cycle = pwm
+ 
+    # Move backward
+    elif speed < 0:
+        IN1.value = False
+        IN2.value = True
+        
+ 
+        ENA.duty_cycle = pwm
+ 
+    # Stop motors
+    elif speed == 0:
+        IN1.value = False
+        IN2.value = False 
+
+# Function to move left wheel
+def move_left_wheel(speed):
+    pwm = int(0xFFFF * speed)
+
+ 
+    # Move forward
+    if speed > 0:
+        IN4.value = True
+        IN3.value = False        
+ 
+        ENB.duty_cycle = pwm
+ 
+    # Move backward
+    elif speed < 0:
+        IN4.value = False
+        IN3.value = True 
+        ENB.duty_cycle = pwm
+ 
+    # Stop motors
+    elif speed == 0:
+        IN4.value = False
+        IN3.value = False        
+ 
+# Function setmode
 def stop():
-    for channel in motor_channels:
-        channel.duty_cycle = 0
-
+    move_right_wheel(0)
+    move_left_wheel(0)
+ 
 # Function to make the robot move forward
-def move_forward():
-    motorR1.duty_cycle = 0          # Set R1 to low
-    motorR2.duty_cycle = 0x7FFF     # Set R2 to 50% duty cycle
-    motorL1.duty_cycle = 0          # Set L1 to low
-    motorL2.duty_cycle = 0x7FFF     # Set L2 to 50% duty cycle
-
+def move_forward(speed):
+    move_right_wheel(speed)
+    move_left_wheel(speed)
+ 
 # Function to make the robot pivot right
-def pivot_right():
-    motorR1.duty_cycle = 0x7FFF     # Set R1 to 50% duty cycle
-    motorR2.duty_cycle = 0          # Set R2 to low
-    motorL1.duty_cycle = 0          # Set L1 to low
-    motorL2.duty_cycle = 0x7FFF     # Set L2 to 50% duty cycle
-
+def pivot_right(speed):
+    move_right_wheel(0)
+    move_left_wheel(speed)
+ 
 # Function to make the robot trace a square
 def move_square():
     # Move forward and pivot right four times, tracing a square
     for i in range(4):
-        move_forward()
+        move_forward(0.5)
         time.sleep(1)
-        pivot_right()
-        time.sleep(0.53)
+        pivot_right(0.2)
+        time.sleep(0.8)
+        print(f'Iteration {i}')
     stop()
-
-
+ 
 # Main function
 def main():
     move_square()
-
 
 # Run the main loop
 if __name__ == '__main__':
